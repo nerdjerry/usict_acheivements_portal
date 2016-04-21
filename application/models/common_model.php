cb7e0bc30a4de64789f04fd16268eaa6a7acf3b3
Aman Arora <er_aman@outlook.com>
2016-04-21 21:51:43 +0530

    added delete function in common_model

--- application/models/common_model.php
+++ application/models/common_model.php
@@ -5,9 +5,26 @@ class Common_model extends CI_Model {
 	{
 		parent::__construct();
 	}
+
 	function add($table_name, $insertData=array()) {
 		$result = $this->db->insert($table_name,$insertData);
 		return $result;
+	}
+	/*
+		table_name : name of the table from which you want to delete
+		$conditions : an array that consists of key value pairs. key represents the column name in the table.
+					multiple conditions can be passed(therefore array).
+
+		eg: conditions['name'] = 'USIT'
+			$table_name = 'Colleges'
+		this would delete all rows in colleges table where name='USIT'
+	*/
+	function delete($table_name, $conditions=array()) {
+		if(count($conditions)>0) {
+			$this->db->where($conditions);
+		}
+		$this->db->delete($table_name); 
+		return true;
 	}
 }
 ?>
