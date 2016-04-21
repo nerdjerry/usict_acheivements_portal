a7590b4d2d972797c288f0c30a89b8cff13ce6db
nerdjerry <psinghal4210@gmail.com>
2016-04-21 21:18:26 +0530

    Common model for achivements,now have method to return no of achievements

---
+++ application/models/achievements.php
@@ -0,0 +1,21 @@
+<?php
+class Achievement_model extends CI_Model {
+	
+	function __construct()
+	{
+		parent::__construct();
+	}
+	
+	public function getStaffAchivementCounts($facultyId){
+		$query = $this->db->where('faculty_id',$facultyId)->get('publications');
+		$count['publications'] = $query->num_rows();
+		$query = $this->db->where('faculty_id',$facultyId)->get('seminars');
+		$count['seminars'] = $query->num_rows();
+		$query = $this->db->where('faculty_id',$facultyId)->get('projects');
+		$count['projects'] = $query->num_rows();
+		$query = $this->db->where('faculty_id',$facultyId)->get('awards');
+		$count['awards'] = $query->num_rows();
+		return $count;
+	}
+}
+?>
