095cbb648209158683a1fb1ed072e34c4019c678
nerdjerry <psinghal4210@gmail.com>
2016-04-22 23:58:33 +0530

    Prevent any user from accessing staff page

--- application/controllers/Achievement.php
+++ application/controllers/Achievement.php
@@ -165,39 +165,44 @@ class Achievement extends CI_Controller {
 	*/
 	/* Controller to display staff achievements*/
 	public function staff($requestedDataType = 1){
-		$userType = 1;
-		$facultyId = get_user_id();
-		$infoType = $requestedDataType;
-		//Loading required models
-		$this->load->model('achievements');
-		$count = $this->achievements->getStaffAchivementCounts($facultyId);
-		$data['noOfPublications'] = $count['publications'];
-		$data['noOfSeminars'] = $count['seminars'];
-		$data['noOfProjects'] = $count['projects'];
-		$data['noOfAwards'] = $count['awards'];
-		$data['infoType'] = $infoType;
-		switch($infoType){
-			case 1:
-				$this->load->model('publications');
-				$data['info'] = $this->publications->getPublications($facultyId);
-				break;
-			case 2:
-				$this->load->model('seminars');
-				$data['info'] = $this->seminars->getSeminars($facultyId);
-				break;
-			case 3:
-				$this->load->model('projects');
-				$data['info'] = $this->projects->getProjects($facultyId);
-				break;
-			case 4:
-				$this->load->model('awards');
-				$data['info'] = $this->awards->getAwards($facultyId,$userType);
-				break;
-			default:
-				redirect('achievement/staff/');
-				break;
-		}
-		$this->load->view('staffAchievements.php',$data);
+		$currentUserType = get_user_type();
+		if($currentUserType == self::FACULTY){
+			$userType = 1;
+			$facultyId = get_user_id();
+			$infoType = $requestedDataType;
+			//Loading required models
+			$this->load->model('achievements');
+			$count = $this->achievements->getStaffAchivementCounts($facultyId);
+			$data['noOfPublications'] = $count['publications'];
+			$data['noOfSeminars'] = $count['seminars'];
+			$data['noOfProjects'] = $count['projects'];
+			$data['noOfAwards'] = $count['awards'];
+			$data['infoType'] = $infoType;
+			switch($infoType){
+				case 1:
+					$this->load->model('publications');
+					$data['info'] = $this->publications->getPublications($facultyId);
+					break;
+				case 2:
+					$this->load->model('seminars');
+					$data['info'] = $this->seminars->getSeminars($facultyId);
+					break;
+				case 3:
+					$this->load->model('projects');
+					$data['info'] = $this->projects->getProjects($facultyId);
+					break;
+				case 4:
+					$this->load->model('awards');
+					$data['info'] = $this->awards->getAwards($facultyId,$userType);
+					break;
+				default:
+					redirect('achievement/staff/');
+					break;
+			}
+			$this->load->view('staffAchievements.php',$data);
+			}else{
+				redirect('/home');
+			}
 	}
 	/*
 		paramas:
