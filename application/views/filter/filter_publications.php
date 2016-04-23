4326cc1450f33380926bb8bf7c3f81d37d3c15f9
Aman Arora <er_aman@outlook.com>
2016-04-23 14:28:41 +0530

    added filter_publication view

---
+++ application/views/filter/filter_publications.php
@@ -0,0 +1,57 @@
+<form id="filter_publications" class="form-horizontal" method="post" action="#" role="form">
+  	<div class="form-group">
+	    <label class="col-sm-2 col-md-2 control-label">Name</label>
+	    <div class="col-sm-10 col-md-8">
+	      <input type="text" class="form-control" name="name" />
+	    </div>
+  	</div>
+  	<div class="form-group">
+	    <label class="col-sm-2 col-md-2 control-label">Designation</label>
+	    <div class="col-sm-10 col-md-8">
+	      	<label class="checkbox-inline"><input type="checkbox" name="professor" value="Professor">Professor</label>
+			<label class="checkbox-inline"><input type="checkbox" name="associate_professor" value="Associate Professor">Associate Professor</label>
+			<label class="checkbox-inline"><input type="checkbox" name="assistant_professor" value="Assistant Professor">Assistant Professor</label>
+	    </div>
+  	</div>
+  	<div class="form-group">
+	    <label class="col-sm-2 col-md-2 control-label">Title</label>
+	    <div class="col-sm-10 col-md-8">
+	      <input type="text" class="form-control" name="title" />
+	    </div>
+  	</div>
+  	<div class="form-group">
+	    <label class="col-sm-2 col-md-2 control-label">Date</label>
+	    <div class="col-sm-5 col-md-4">
+	    	<input type="text" class ="form-control start_date" name="start_date" id="start_date" placeholder="Start Date" />
+	    </div>
+	    <div class="col-sm-5 col-md-4">
+	    	<input type="text" class ="form-control end_date" name="end_date" id="end_date" placeholder="End date" />
+	    </div>
+  	</div>
+  	<div class="form-group">
+	    <label class="col-sm-2 col-md-2 control-label">Journal Name</label>
+	    <div class="col-sm-10 col-md-8">
+	      <input type="text" class="form-control" name="journal_name" />
+	    </div>
+  	</div>
+  	<div class="form-group">
+	    <label class="col-sm-2 col-md-2 control-label">Presented in</label>
+	    <div class="col-sm-10 col-md-8">
+	      	<label class="checkbox-inline"><input type="checkbox" name="journal" value="Journal">Journal</label>
+			<label class="checkbox-inline"><input type="checkbox" name="conference" value="Conference">Conference</label>
+	    </div>
+  	</div>
+  	<div class="form-group">
+	    <label class="col-sm-2 col-md-2 control-label">Presented at</label>
+	    <div class="col-sm-10 col-md-8">
+	      	<label class="checkbox-inline"><input type="checkbox" name="national" value="National">National</label>
+			<label class="checkbox-inline"><input type="checkbox" name="international" value="International">International</label>
+	    </div>
+  	</div>
+  	<div class="form-group form-actions">
+    	<div class="col-sm-offset-2 col-sm-10">
+      		<button type="submit" class="btn btn-success">Save customer</button>
+      		<button type="submit" class="btn btn-danger">Export</button>
+		</div>
+  	</div>
+</form>
