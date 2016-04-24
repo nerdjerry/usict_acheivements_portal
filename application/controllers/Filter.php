7ddac1c1c8902607de2a21faaf60cdc02f2d5b96
nerdjerry <psinghal4210@gmail.com>
2016-04-24 16:51:31 +0530

    Work on exporting.Not Complete.

--- application/controllers/Filter.php
+++ application/controllers/Filter.php
@@ -4,9 +4,15 @@ defined('BASEPATH') or exit('No direct script access allowed');
 class Filter extends CI_COntroller{
 
 	private $currentUseType;
+	private $count;
+	private $filter;
 	public function __construct(){
 		parent::__construct();
 		$this->currentUserType = get_user_type();
+		//Get number of achievements
+		$this->load->model('achievements');
+		$this->count = $this->achievements->getAllStaffAchievementCounts();
+		$this->filter = null;
 	}
 
 	public function publication(){
@@ -23,29 +29,56 @@ class Filter extends CI_COntroller{
 			$condition['isConference'] = $this->input->post('conference') != null ? $this->input->post('conference') : '';
 			$condition['isNational'] = $this->input->post('national') != null ? $this->input->post('national'): '';
 			$condition['isInternational'] = $this->input->post('international') != null ?$this->input->post('international')  : '';
+			$this->filter = $condition;
+			$resultType = $this->input->post('results') != null ? $this->input->post('results') : 'view';
 			$this->load->model('publications');
-			//Get number of achievements
-			$this->load->model('achievements');
-			$count = $this->achievements->getAllStaffAchievementCounts();
-			$data['noOfPublications'] = $count['publications'];
-			$data['noOfSeminars'] = $count['seminars'];
-			$data['noOfProjects'] = $count['projects'];
-			$data['noOfAwards'] = $count['awards'];
+			$data['noOfPublications'] = $this->count['publications'];
+			$data['noOfSeminars'] = $this->count['seminars'];
+			$data['noOfProjects'] = $this->count['projects'];
+			$data['noOfAwards'] = $this->count['awards'];
 			$data['infoType'] = 1;
 			$data['requestedUserType'] = 1;
 			$data['results'] = true;
-			//Pagination
-			$totalRows = $count['publications'];
-			$perPage = 30;
-			$uriSegment = 3;
-			$page=$this->uri->segment(3) != null? $this->uri->segment(3) : 1;
-			//Get data from model
-			$data['info'] = $this->publications->filteredPublications($condition,$perPage,$page);
-			$data['links'] = $this->doPagination('/filter/publication/',$totalRows,$perPage,$uriSegment);;
-			$this->load->view('admin',$data);
+			if($resultType == 'view'){
+				//Pagination
+				$totalRows = $this->count['publications'];
+				$perPage = 30;
+				$uriSegment = 3;
+				$page=$this->uri->segment(3) != null? $this->uri->segment(3) : 1;
+				//Get data from model
+				$data['info'] = $this->publications->filteredPublications($condition,$perPage,$page);
+				$data['links'] = $this->doPagination('/filter/publication/',$totalRows,$perPage,$uriSegment);
+				$this->load->view('admin',$data);
+			}else{
+				show_error("Exporting");
+				redirect('/filter/exportPublication');
+			}
+			
 		}else{
 			redirect('/home');
 		}
+	}
+
+	public function exportPublication(){
+		if(!is_null($this->filter)){
+			$this->load->library('excel');
+			//Activate worksheet number 1
+			$this->excel->setActiveSheetIndex(0);
+			$this->excel->getActiveSheet()->setTitle('Results');
+			$this->load->model('publications');
+			$resultsData = $this->publications->filteredPublications($this->filter,$this->count['publications'],1);
+			$this->excel->getActiveSheet()->fromArray($resultsData);
+			$fileName = 'Publications.xlsx';
+			header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
+			header('Content-Disposition: attachment;filename="'.$fileName.'"');
+			header('Cache-Control: max-age=0');
+			$objWriter = PHPExcel_IOFactory::createWriter($this->excel, 'Excel2007');
+			$objWriter->save('php://output');
+			redirect('filter/publication');
+		}else{
+			show_error("Message");
+		}
+		
 	}
 
 	private function doPagination($baseUrl,$totalRows,$perPage,$uriSegment){
