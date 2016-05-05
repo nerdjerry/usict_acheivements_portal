cfe3af9896e9714b458fbb3a55d903cc17b2586b
nerdjerry <psinghal4210@gmail.com>
2016-05-06 02:49:40 +0530

    Add export for student achievement

--- application/controllers/Export.php
+++ application/controllers/Export.php
@@ -182,41 +182,35 @@ class Export extends CI_Controller{
 		}	
 	}
 
-	/*public function exportAchievement(){
-		if(isset($_SESSION['']) && $this->currentUserType == 0){
+	public function exportAchievement(){
+		if(isset($_SESSION['achievement']) && $this->currentUserType == 0){
 			$this->setupFile();
 			//This contains all attributes we need in header
-			$fieldNames= array('Name', 'Designation', );	
+			$fieldNames= array('Name', 'Branch', 'Student Year', 'Details', 'Year of Achievement' );	
 			$this->setHeader($fieldNames);	
 			//Load model,get data based on filter saved in session
-			$this->load->model('');
-			$resultsData = $this->->($_SESSION[''],$this->count[''],1);
+			$this->load->model('student');
+			//Get Student achievements count
+			$this->count = $this->achievements->getAllStudentAchievementCounts();
+			$resultsData = $this->student->filteredAchievements($_SESSION['achievement'],$this->count['achievements'],1);
 			$index = 2;
 			//Set values for each rows as per result returned from model
 			foreach ($resultsData as $result) {
 				$this->excel->getActiveSheet()->setCellValue("A".$index,$result['name']);
-				$this->excel->getActiveSheet()->setCellValue("B".$index,$result['designation']);
-				$this->excel->getActiveSheet()->setCellValue("C".$index,$result['']);
-				$this->excel->getActiveSheet()->setCellValue("D".$index,$result['']);
-				$this->excel->getActiveSheet()->setCellValue("E".$index,$result['']);
-				$this->excel->getActiveSheet()->setCellValue("F".$index,$result['']);
-				$this->excel->getActiveSheet()->setCellValue("G".$index,$result['']);
-				$this->excel->getActiveSheet()->setCellValue("H".$index,$result['']);
-				$this->excel->getActiveSheet()->setCellValue("I".$index,$result['']);
-				$this->excel->getActiveSheet()->setCellValue("J".$index,$result['']);
-				$this->excel->getActiveSheet()->setCellValue("K".$index,$result['']);
-				$this->excel->getActiveSheet()->setCellValue("L".$index,$result['']);
-				$this->excel->getActiveSheet()->setCellValue("M".$index,$result['']);								
+				$this->excel->getActiveSheet()->setCellValue("B".$index,$result['branch']);
+				$this->excel->getActiveSheet()->setCellValue("C".$index,$result['studentYear']);
+				$this->excel->getActiveSheet()->setCellValue("D".$index,$result['details']);
+				$this->excel->getActiveSheet()->setCellValue("E".$index,$result['year']);							
 				$index++;
 			}
-			$this->wrapAndCenterText('',$index);
-			$fileName = 'USIT'.date('d-m-Y').'.xls';
+			$this->wrapAndCenterText('E',$index);
+			$fileName = 'USITStudentAchievements ('.date('d-m-Y').')'.'.xls';
 			$this->sendFile($fileName);
-			redirect('filter/');
+			redirect('filter/achievement');
 		}else{
 			show_error("Action not Allowed");
 		}	
-	}*/
+	}
 
 	private function setupFile(){
 		//Load the PHP EXCEL library instance
